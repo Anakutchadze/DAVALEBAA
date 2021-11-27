79a83cd373a3b2a8acef7dc5c21b74e8ec38c00e
John Doe <john@doe.org>
2021-11-27 15:38:49 -0800

    HERO

---
+++ app/src/main/java/com/example/davaleba4/fragments/PhotoFragment.kt
@@ -0,0 +1,17 @@
+package com.example.davaleba4.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.davaleba4.R
+
+class PhotoFragment : Fragment(R.layout.fragment_images) {
+
+    private lateinit var notesButton: Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        }
+    }
